38ee0c316be5dcde27f846662dba83469719a5e1
m.tolmats <mihail910@mail.ru>
2017-04-06 17:09:54 +0300

    menu_and_start_right-settings_gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('chlen', function(){
+    return gulp.src('app/scss/style.scss')
+        .pipe(sass())
+		.pipe(autoprefixer({
+			cascade: true,
+			browsers: ['not ie <= 8', 'Firefox > 39', 'iOS > 6', 'ios_saf > 6', 'Chrome > 39']
+		}))
+		.pipe(cleanCSS())
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('default', function(){
+    gulp.watch('app/scss/modules/*.scss', ['chlen']);
+})
